200c9d164f337d5bd103dd9f0a192122002b73fa
EmpyTM <37101108+EmpyTM@users.noreply.github.com>
2024-08-11 23:05:49 -0500

    Update scenebuilder_json_con.lua
    minor cleanup

--- client/scenebuilder_json_con.lua
+++ client/scenebuilder_json_con.lua
@@ -1,42 +1,36 @@
 local filepath = "models/scene_building"
-if file.Exists(filepath, "GAME") == false then return end
+if not file.Exists( filepath, "GAME" ) then return end
 
-local _, folders = file.Find(filepath .. "/*", "GAME")
+local _, folders = file.Find( filepath .. "/*", "GAME" )
 local to_json = {}
 
 local ent = NULL
 
-for _, fo in ipairs(folders) do
+for _, fo in ipairs( folders ) do
 	to_json[fo] = {}
 	
-	local cur = ("%s/%s"):format(filepath, fo)
-	local stuff = file.Find(cur .. "/*", "GAME")
+	local cur = string.format( "%s/%s", filepath, fo )
+	local stuff = file.Find( cur .. "/*", "GAME" )
 
-	for i, v in ipairs(stuff) do
-		if v:EndsWith(".mdl") == false then continue end
+	for i, v in ipairs( stuff ) do
+		if not v:EndsWith( ".mdl" ) then continue end
 
-		local path = ("%s/%s"):format(cur, v)
+		local path = string.format( "%s/%s", cur, v )
+		
 		ent = ClientsideModel(path)
-
+		
 		local model_info = {}
 		local mmin, mmax = ent:GetModelBounds()
-		model_info.min = mmin:ToTable()
-		model_info.max = mmax:ToTable()
-		model_info.skins = ent:SkinCount()
-		model_info.path = path
-		model_info.control_points = {} -- sad
+		model_info[ "min" ] = mmin:ToTable()
+		model_info[ "max" ] = mmax:ToTable()
+		model_info[ "skin_count" ] = ent:SkinCount()
+		model_info[ "model_path" ] = path
+		model_info[ "attachments" ] = {} -- sad
+		model_info[ "bodygroup_count" ] = ent:GetNumBodyGroups()
 
-		local bodygroups = ent:GetNumBodyGroups()
-		if bodygroups > 1 then
-			model_info.bodygroups = bodygroups
-		end
-
-		local trimmed = stuff[i]:TrimRight(".mdl")
-		to_json[fo][trimmed] = model_info
+		to_json[ fo ][ stuff[ i ]:TrimRight( ".mdl" ) ] = model_info
 	end
 end
 
 ent:Remove()
-
-local json = util.TableToJSON(to_json, true)
-file.Write("scenebuilder.json", json)
+file.Write( "scenebuilder.json", util.TableToJSON( to_json, true ) )
