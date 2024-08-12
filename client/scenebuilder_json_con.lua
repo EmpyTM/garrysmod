0e5d59e1a5a7d937a995390a468574b485ef3405
EmpyTM <37101108+EmpyTM@users.noreply.github.com>
2024-08-11 23:14:36 -0500

    don't convert mins and maxs to sequential table

--- client/scenebuilder_json_con.lua
+++ client/scenebuilder_json_con.lua
@@ -7,29 +7,28 @@ local to_json = {}
 local ent = NULL
 
 for _, fo in ipairs( folders ) do
-	to_json[fo] = {}
-	
-	local cur = string.format( "%s/%s", filepath, fo )
-	local stuff = file.Find( cur .. "/*", "GAME" )
+    to_json[fo] = {}
+    
+    local cur = string.format( "%s/%s", filepath, fo )
+    local stuff = file.Find( cur .. "/*", "GAME" )
 
-	for i, v in ipairs( stuff ) do
-		if not v:EndsWith( ".mdl" ) then continue end
+    for i, v in ipairs( stuff ) do
+        if not v:EndsWith( ".mdl" ) then continue end
+        local path = string.format( "%s/%s", cur, v )
+        
+        ent = ClientsideModel(path)
+        
+        local model_info = {}
+        local mmin, mmax = ent:GetModelBounds()
+        model_info[ "mins" ] = { x = mmin.x, y = mmin.y, z = mmin.z }
+        model_info[ "maxs" ] = { x = mmax.x, y = mmax.y, z = mmax.z }
+        model_info[ "skin_count" ] = ent:SkinCount()
+        model_info[ "model_path" ] = path
+        model_info[ "attachments" ] = {} -- sad
+        model_info[ "bodygroup_count" ] = ent:GetNumBodyGroups()
 
-		local path = string.format( "%s/%s", cur, v )
-		
-		ent = ClientsideModel(path)
-		
-		local model_info = {}
-		local mmin, mmax = ent:GetModelBounds()
-		model_info[ "min" ] = mmin:ToTable()
-		model_info[ "max" ] = mmax:ToTable()
-		model_info[ "skin_count" ] = ent:SkinCount()
-		model_info[ "model_path" ] = path
-		model_info[ "attachments" ] = {} -- sad
-		model_info[ "bodygroup_count" ] = ent:GetNumBodyGroups()
-
-		to_json[ fo ][ stuff[ i ]:TrimRight( ".mdl" ) ] = model_info
-	end
+        to_json[ fo ][ stuff[ i ]:TrimRight( ".mdl" ) ] = model_info
+    end
 end
 
 ent:Remove()
