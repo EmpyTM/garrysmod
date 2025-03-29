6db22c7893426c635bc5f25dba4cd5e9f52b1c0f
EmpyTM <37101108+EmpyTM@users.noreply.github.com>
2025-03-29 15:03:59 -0500

    Reformatted code

--- shared/timer_extension.lua
+++ shared/timer_extension.lua
@@ -1,78 +1,83 @@
-local timer_create = timer.Create
-local timer_pause = timer.Pause
-local timer_unpause = timer.UnPause
-local timer_remove = timer.Remove
-local timer_adjust = timer.Adjust
-local timer_stop = timer.Stop
-local timer_start = timer.Start
+local timers = {};
 
-timer.timers = {}
+local timer_create = timer.Create;
 
 function timer.Create(identifier, delay, repetitions, func)
-    timer.timers[identifier] = {
+    timers[identifier] = {
         delay = delay,
         repetitions = repetitions,
         func = func,
         status = "running"
-    }
+    };
     
-    timer_create(identifier, delay, repetitions, func)
+    timer_create(identifier, delay, repetitions, func);
 end
+
+local timer_remove = timer.Remove;
 
 function timer.Remove(identifier)
-    timer.timers[identifier] = timer.Exists(identifier) and nil
-    
-    timer_remove(identifier)
+    if (identifier == "timers_update") then
+        error("Tried to remove \"timer_update\" timer!");
+    end
+
+    timers[identifier] = timer.Exists(identifier) and nil;    
+    timer_remove(identifier);
 end
+
+local timer_pause = timer.Pause;
 
 function timer.Pause(identifier)
-    timer.timers[identifier].status = timer.Exists(identifier) and "paused"
-    
-    return timer_pause(identifier)
+    timers[identifier].status = timer.Exists(identifier) and "paused";
+    return timer_pause(identifier);
 end
+
+local timer_unpause = timer.UnPause;
 
 function timer.UnPause(identifier)
-    timer.timers[identifier].status = timer.Exists(identifier) and "running"
-    
-    return timer_unpause(identifier)
+    timers[identifier].status = timer.Exists(identifier) and "running";
+    return timer_unpause(identifier);
 end
 
+local timer_adjust = timer.Adjust;
+
 function timer.Adjust(identifier, delay, repetitions, func)
-    if not timer.Exists(identifier) then return end
+    if (not timer.Exists(identifier)) then return end
     
-    local t = timer.timers[identifier]
-    timer.timers[identifier] = {
+    local t = timers[identifier];
+    timers[identifier] = {
         delay = delay,
         repetitions = repetitions or t.repetitions,
         func = func or t.func,
         status = t.status
-    }
+    };
 
-    return timer_adjust(identifier, delay, repetitions, func)
+    return timer_adjust(identifier, delay, repetitions, func);
 end
+
+local timer_stop = timer.Stop;
 
 function timer.Stop(identifier)
-    timer.timers[identifier].status = timer.Exists(identifier) and "awaiting"
-    
-    return timer_stop(identifier)
+    timers[identifier].status = timer.Exists(identifier) and "awaiting";
+    return timer_stop(identifier);
 end
+
+local timer_start = timer.Start;
 
 function timer.Start(identifier)
-    timer.timers[identifier].status = timer.Exists(identifier) and "running"
-
-    return timer_start(identifier)
+    timers[identifier].status = timer.Exists(identifier) and "running";
+    return timer_start(identifier);
 end
 
-function timer.GetTable()
-    return timer.timers
+function timer.get_table()
+    return table.Copy(timers);
 end
 
-function timer.Status(identifier)
-    return timer.Exists(identifier) and timer.timers[identifier].status
+function timer.status(identifier)
+    return timer.Exists(identifier) and timers[identifier].status;
 end
 
-timer.Create("timer.timers", 1, 0, function()
-    for identifier in pairs(timer.timers) do
-        timer.timers[identifier] = timer.Exists(identifier) and timer.timers[identifier] or nil
+timer.Create("timers_update", 1, 0, function()
+    for identifier in pairs(timers) do
+        timers[identifier] = timer.Exists(identifier) and timers[identifier] or nil;
     end
 end)
