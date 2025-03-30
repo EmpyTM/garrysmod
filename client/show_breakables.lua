78b77ae9428d17d7f40d30f5b221255296f0dd48
EmpyTM <37101108+EmpyTM@users.noreply.github.com>
2025-03-30 14:41:51 -0500

    use console command instead of the more complex chat-based command

--- client/show_breakables.lua
+++ client/show_breakables.lua
@@ -1,55 +1,41 @@
--- crappy hack
-_R.Entity.IsBreakable = function(self)
-	return self:GetMaxHealth() >= 1 and self:Health() ~= 0
-end
-
 local tag = "breakables_visualizer"
-local breakables = { color = Color(255, 127, 0) }
+
+local breakables = {}
+setmetatable(breakables, {__mode = "v"})
+
+local breakables_color = Color(255, 127, 0)
+
+local function is_breakable(ent)
+	return ent:GetMaxHealth() >= 1 and ent:Health() ~= 0
+end
 
 for i, v in ipairs(ents.FindByClass("prop_physics")) do
-	if v:IsBreakable() then breakables[#breakables + 1] = v end
+	if is_breakable(v) then
+		breakables[#breakables + 1] = v
+	end
 end
 
-local function on_ent_create(ent)
-	if not ent:IsBreakable() then return end
-	if not ent:IsValid() then return end
+hook.Add("OnEntityCreated", tag, function(ent)
+	if not (ent:IsValid() and is_breakable(ent)) then return end
 	
 	timer.Simple(0, function()
 		if ent:GetClass() ~= "prop_physics" then return end
 		breakables[#breakables + 1] = ent
 	end)
+end)
+
+local function draw_halos()
+	halo.Add(breakables, breakables_color, 3, 3, 1, true, false)
 end
 
-local function on_ent_remove(ent)
-	if table.HasValue(breakables, ent) then table.RemoveByValue(breakables, ent) end
-end
+local showing = false
 
-hook.Add("OnEntityCreated", tag, on_ent_create)
-hook.Add("EntityRemoved", tag, on_ent_remove)
-
-local function halo_draw()
-	halo.Add(breakables, breakables.color, 3, 3, 1, true, false)
-end
-
-local command = {
-	showbreak = function(bool)
-		if bool == 1 then
-			hooks.PreDrawHalos[tag] = halo_draw
-		elseif bool == 0 then
-			hooks.PreDrawHalos[tag] = nil
-		else
-			return
-		end
+concommand.Add("toggle_breakables_show", function(ply, cmd, args)
+	if not showing then
+		hook.Add("PreDrawHalos", tag, draw_halos)
+		showing = true
+	else
+		hook.Remove("PreDrawHalos", tag, draw_halos)
+		showing = false
 	end
-}
-
-hook.Add("OnPlayerChat", tag, function(ply, txt)
-	if ply ~= LocalPlayer() then return end
-    
-	local args = txt:Split(" ")
-	local a = args[1]:TrimLeft(args[1]:sub(1, 1))
-	
-	if not command[a] then return end
-    
-	command[a](tonumber(args[2]))
 end)
